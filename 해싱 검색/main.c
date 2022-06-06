906707e27af62d4d74bb9dcd88de6f038bdd74f5
주영 유 <qlenfrl0577@naver.com>
2022-06-06 20:22:02 +0900

    해싱 검색 알고리즘 코드 수정

--- 해싱 검색/main.c
+++ 해싱 검색/main.c
@@ -11,7 +11,7 @@ typedef struct Node {
 	struct Node *next;
 } Node;
 
-Node store[5];
+Node* bucket[5];
 
 Node* getNewNode(element data) {
 	Node *newNode = (Node *)malloc(sizeof(Node));
@@ -25,27 +25,72 @@ int toHashCode(char c) {
 	return c % 5;
 }
 
-void insert(element data) {
-	Node *newNode = getNewNode(data);
+int search(element data) {
 	int hashCode = toHashCode(data);
-
-	newNode->next = &store[hashCode];
-	store[hashCode] = *newNode;
-}
-
-void search(element data) {
-	int hashCode = toHashCode(data);
-	Node *node = &store[hashCode];
+	Node *node = bucket[hashCode];
 
 	while (node != NULL) {
 		if (node->data == data) {
-			printf("검색 성공\n\n");
-			return;
+			return TRUE;
 		}
 		node = node->next;
 	}
+	return FALSE;
+}
 
-	printf("검색 실패\n\n");
+int insert(element data) {
+	Node *newNode = getNewNode(data);
+	int hashCode = toHashCode(data);
+
+	if (search(data)) {
+		printf("이미 값이 있음\n\n");
+		return 0;
+	}
+	else {
+		newNode->next = bucket[hashCode];
+		bucket[hashCode] = newNode;
+	}
+	return 1;
+}
+
+void delete(element data) {
+	int hashCode = toHashCode(data);
+
+	Node *node = bucket[hashCode];
+	Node *last = NULL;
+
+	while (node != NULL) {
+		if (node->data == data) {
+			if (last == NULL) {
+				bucket[hashCode] = node->next;
+			}
+			else {
+				last->next = node->next;
+			}
+
+			free(node);
+			printf("%c 제거 완료\n\n", data);
+			return;
+		}
+		last = node;
+		node = node->next;
+	}
+	printf("값이 없음\n\n");
+}
+
+void printStore() {
+	for (int i = 0; i < 5; i++) {
+		printf("Bucket[%d]: [ ", i);
+
+		Node *node = bucket[i];
+
+		while (node != NULL) {
+			printf("%c ", node->data);
+			node = node->next;
+		}
+		printf("]\n");
+	}
+	printf("\n");
 }
 
 void main() {
@@ -53,22 +98,38 @@ void main() {
 	char input;
 
 	while (TRUE) {
-		printf("1 삽입, 2 검색, 3 종료: ");
+		printf("1 삽입, 2 검색, 3: 삭제, 4 종료: ");
 		scanf_s("%d", &selector);
 		getchar();
+		printf("\n");
 
 		switch (selector) {
 		case 1:
 			printf("삽입할 문자: ");
 			scanf_s("%c", &input);
-			insert(input);
+			printf("\n");
+
+			if (insert(input)) {
+				printStore();
+			}
 			break;
 		case 2:
 			printf("검색할 문자: ");
 			scanf_s("%c", &input);
-			search(input);
+			printf("\n");
+
+			printf(search(input) ? "검색 성공" : "검색 실패");
+			printf("\n\n");
 			break;
 		case 3:
+			printf("삭제할 문자: ");
+			scanf_s("%c", &input);
+			printf("\n");
+
+			delete(input);
+			printStore();
+			break;
+		case 4:
 			return;
 		default:
 			break;
